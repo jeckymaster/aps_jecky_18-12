a0a996d50071e4d53991fe0980c3f920007c4599
jeckymaster <hemin59567@gmail.com>
2022-12-29 12:01:39 +0530

    completed data transformation

--- sensor/entity/config_entity.py
+++ sensor/entity/config_entity.py
@@ -5,6 +5,9 @@ from datetime import datetime
 FILE_NAME = "sensor.csv"
 TRAIN_FILE_NAME = "train.csv"
 TEST_FILE_NAME = "test.csv"
+TRANSFORMER_OBJECT_FILE_NAME = "transformer.pkl"
+TARGET_ENCODER_OBJECT_FILE_NAME="target_encoder.pkl"
+MODEL_FILE_NAME = "model.pkl"
 
 class TrainingPipelineConfig:
 
@@ -42,7 +45,14 @@ class DataValidationConfig:
         self.missing_threshold:float = 0.2
         self.base_file_path = os.path.join("aps_failure_training_set1.csv")
             
-class DataTransformationConfig:...
+class DataTransformationConfig:
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        self.data_transfirmation_dir = os.path.join(training_pipeline_config.artifact_dir, "data_transfirmation")
+        self.transform_object_path = os.path.join(self.data_transfirmation_dir,"transformer",TRANSFORMER_OBJECT_FILE_NAME)
+        self.transformed_train_path = os.path.join(self.data_transfirmation_dir,"transformered",TRAIN_FILE_NAME)
+        self.transformed_test_path = os.path.join(self.data_transfirmation_dir,"transformered",TEST_FILE_NAME)
+        self.target_encoder_path = os.path.join(self.data_transfirmation_dir,"target_encoder",TARGET_ENCODER_OBJECT_FILE_NAME)
+
 class ModelTrainerConfig:...
 class ModelEvaluationConfig:...
 class ModelPusherConfig:...
